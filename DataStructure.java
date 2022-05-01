0115c10d3e6cb5cba3e337099a1c7bbc2c159f9a
rohitmartand123 <rohitmartand274@gmail.com>
2022-05-01 22:44:48 +0530

    Program to delete last element

--- DataStructure.java
+++ DataStructure.java
@@ -1,8 +1,8 @@
 package com.bridgelabz.linkedlist;
 /*
- * Rohit
- * Ability to delete the first element in the LinkedList of sequence 56->30->70
- * Final Sequence: 30->70
+ * @author Rohit
+ *  Ability to delete the last element in the LinkedList of sequence 56->30->70
+ * Final Sequence: 56->30
  */
 public class DataStructure {
 	// Add element
@@ -53,7 +53,22 @@ public class DataStructure {
 			list.add(30);
 			list.add(56);
 			list.printNode();
+
 			list.pop();
+			list.printNode();
+		}
+
+		// Delete last element
+		public static void popLast() {
+			MyLinkedList list = new MyLinkedList();
+			list.add(70);
+			list.add(30);
+			list.add(56);
+			System.out.println("\nBefore deleting last node : ");
+			list.printNode();
+
+			System.out.println("\nAfter deleting last node : ");
+			list.popLast();
 			list.printNode();
 		}
 
@@ -76,6 +91,10 @@ public class DataStructure {
 			// Calling popFirst method to delete first node
 			System.out.println("Delete first node : ");
 			popFirst();
+
+			// Calling popLast method to last first node
+			System.out.println("Delete last node : ");
+			popLast();
 	}
 
 }
