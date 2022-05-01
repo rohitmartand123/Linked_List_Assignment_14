62345674167e262af764a8a30ad6525e4e088d09
rohitmartand123 <rohitmartand274@gmail.com>
2022-05-01 21:21:57 +0530

    Program to creat linked list by appending 30 and 56 to 70

--- DataStructure.java
+++ DataStructure.java
@@ -1,7 +1,7 @@
 package com.bridgelabz.linkedlist;
 /*
  * Rohit
- * create a simple Linked List of 56, 30 and 70
+ * Ability to create Linked List by appending 30 and 70 to 56
  */
 public class DataStructure {
 	// Add element
@@ -16,12 +16,31 @@ public class DataStructure {
 			// print node
 			list.printNode();
 		}
+
+		// Append Method
+		public static void append() {
+			MyLinkedList list = new MyLinkedList();
+
+			// append node
+			list.append(56);
+			list.append(30);
+			list.append(70);
+
+			// print node
+			list.printNode();
+		}
+		
 	public static void main(String[] args) {
 		System.out.println("-----------Welcome to linked list program---------");
 		System.out.println();
 
+		// Calling Add methods
 		System.out.println("Add Elements in List : ");
 		add();
+
+		// Calling Append Method
+		System.out.println("Appended element of Linked List :");
+		append();
 	}
 
 }
