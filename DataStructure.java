17c8e64e706cdf4bef03dd8d44562c2ce725ff3f
rohitmartand123 <rohitmartand274@gmail.com>
2022-05-01 22:32:05 +0530

    Program to insert 30 between 56 and 70

--- DataStructure.java
+++ DataStructure.java
@@ -1,18 +1,18 @@
 package com.bridgelabz.linkedlist;
 /*
  * Rohit
- * Ability to create Linked List by appending 30 and 70 to 56
+ * Ability to insert 30 between 56 and 70
  */
 public class DataStructure {
 	// Add element
 		public static void add() {
 			// create object
 			MyLinkedList list = new MyLinkedList();
-			
+
 			list.add(70);
 			list.add(30);
 			list.add(56);
-			
+
 			// print node
 			list.printNode();
 		}
@@ -29,18 +29,37 @@ public class DataStructure {
 			// print node
 			list.printNode();
 		}
-		
-	public static void main(String[] args) {
-		System.out.println("-----------Welcome to linked list program---------");
-		System.out.println();
 
-		// Calling Add methods
-		System.out.println("Add Elements in List : ");
-		add();
+		// Insert node in LinkList
+		public static void insertNode() {
+			MyLinkedList list = new MyLinkedList();
 
-		// Calling Append Method
-		System.out.println("Appended element of Linked List :");
-		append();
+			// insert node
+			list.append(56);
+			list.append(70);
+			System.out.println("Before Inserting : ");
+			list.printNode();
+			list.insertNode(list.head, 30);
+			System.out.println("After Inserting : ");
+			// print node
+			list.printNode();
+		}
+
+		public static void main(String[] args) {
+			System.out.println("-----------Welcome to linked list program---------");
+			System.out.println();
+
+			// Calling Add methods
+			System.out.println("Add Elements in List : ");
+			add();
+
+			// Calling Append Method
+			System.out.println("Appended element of Linked List :");
+			append();
+
+			// Calling Insert Method
+			System.out.println("Insert 30 in between 56 and 70");
+			insertNode();
 	}
 
 }
